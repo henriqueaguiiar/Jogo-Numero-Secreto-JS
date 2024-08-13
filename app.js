8816e591124cbe0eae73a2bbb56a98c572e32b23
Henrique Aguiar <henrique.aguiar1998@gmail.com>
2024-08-13 13:18:59 -0300

    Lógica-Inicial-JS

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Bem vindo ao Jogo do Número Secreto");
+let numeroSecreto = 5;
+let inputNumero = prompt("Escolha um número entre 1 e 30: ");
+
+if (numeroSecreto == inputNumero) {
+  alert("Você Acertou o Número Secreto");
+  console.log("Isso ai você descobriu o número secreto!");
+}
